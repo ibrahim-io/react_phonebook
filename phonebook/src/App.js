1e1336aa9deb93346407fffc9c17f9209928f7d0
Ibrahim Ibrahim <ii120@ic.ac.uk>
2022-03-28 11:15:09 +0100

    feat<App.js>: Added persons array to be mapped and displayed on screen

--- phonebook/src/App.js
+++ phonebook/src/App.js
@@ -1,8 +1,35 @@
+import { useState } from 'react'
 
-function App() {
+const Person = ({name}) => {
+  return (
+    <li>{name}</li>
+  )
+}
+
+const App = () => {
+  let id = 1
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+  const [newName, setNewName] = useState('')
+
   return (
     <>
-      <h1>Phonebook App</h1>
+      <h2>Phonebook</h2>
+      <form>
+        <div>
+          name: <input />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <ul>
+        {persons.map(person =>
+          <Person name={person.name} key={id++} />  
+        )}
+      </ul>
     </>
   )
 }
