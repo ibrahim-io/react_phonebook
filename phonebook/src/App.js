2306510ffc67a416ba6eebc135c058643a0c4084
Ibrahim Ibrahim <ii120@ic.ac.uk>
2022-03-28 11:23:06 +0100

    feat<App.js>: Added functionality to adding new persons to the screen

--- phonebook/src/App.js
+++ phonebook/src/App.js
@@ -7,27 +7,36 @@ const Person = ({name}) => {
 }
 
 const App = () => {
-  let id = 1
   const [persons, setPersons] = useState([
     { name: 'Arto Hellas' }
   ]) 
   const [newName, setNewName] = useState('')
+
+  const handleInputChange = (event) => {
+    setNewName(event.target.value)
+  }
+
+  const addPerson = (event) => {
+    event.preventDefault()
+    setPersons(persons.concat({name: newName}))
+    setNewName('')
+  }
 
   return (
     <>
       <h2>Phonebook</h2>
       <form>
         <div>
-          name: <input />
+          name: <input value={newName} onChange={handleInputChange}/>
         </div>
         <div>
-          <button type="submit">add</button>
+          <button type="submit" onClick={addPerson}>add</button>
         </div>
       </form>
       <h2>Numbers</h2>
       <ul>
         {persons.map(person =>
-          <Person name={person.name} key={id++} />  
+          <Person name={person.name} key={person.name} />  
         )}
       </ul>
     </>
